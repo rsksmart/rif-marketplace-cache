a52e341e213355d0a1865904b738bd61fef81ab0
Adam Uhlíř <adam@uhlir.dev>
2020-01-30 14:29:28 +0100

    feat: sequelize integration

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ import middleware from './middleware';
 import services from './services';
 import appHooks from './app.hooks';
 import channels from './channels';
+import sequelize from './sequelize';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const app: Application = express(feathers());
@@ -35,6 +36,8 @@ app.use('/', express.static(app.get('public')));
 // Set up Plugins and providers
 app.configure(express.rest());
 app.configure(socketio());
+
+app.configure(sequelize);
 
 // Configure other middleware (see `middleware/index.js`)
 app.configure(middleware);
---
+++ src/sequelize.ts
@@ -0,0 +1,34 @@
+import { Sequelize } from 'sequelize-typescript'
+import { Application } from './declarations'
+import path from 'path'
+
+export default function (app: Application): void {
+  const dbSettings = Object.assign({
+    models: [path.join(__dirname, '/models/**/*.model.ts')],
+    modelMatch: (filename: string, member: string): boolean => {
+      return filename.substring(0, filename.indexOf('.model')) === member.toLowerCase()
+    }
+  }, app.get('db'))
+
+  const sequelize = new Sequelize(dbSettings)
+  const oldSetup = app.setup
+
+  app.set('sequelizeClient', sequelize)
+
+  app.setup = function (...args): ReturnType<Application['setup']> {
+    const result = oldSetup.apply(this, args)
+
+    // Set up data relationships
+    const models = sequelize.models
+    Object.keys(models).forEach(name => {
+      if ('associate' in models[name]) {
+        (models[name] as any).associate(models)
+      }
+    })
+
+    // Sync to the database
+    app.set('sequelizeSync', sequelize.sync())
+
+    return result
+  }
+}
