9cc7d25cb643f50470a27d3363704ddc05c90e89
Leandro Yalet <leandro@iovlabs.org>
2020-04-09 16:08:26 -0300

    chore: [WIP] added rns-registry and reverse-resolver contracts events

--- src/rns/models/domain.model.ts
+++ src/rns/models/domain.model.ts
@@ -11,14 +11,14 @@ import { Op } from 'sequelize'
 @Table({ freezeTableName: true, tableName: 'rns_domain', timestamps: false })
 export default class Domain extends Model {
   @Column({ primaryKey: true, type: DataType.STRING })
-  ownerAddress!: string
-
-  @Column
   tokenId!: string
 
   @Column
-  name!: string
+  ownerAddress: string
+
+  @Column
+  name: string
 
   @Column({ type: DataType.DATE })
-  expirationDate!: date
+  expirationDate: date
 }
