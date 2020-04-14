49c2ba1ed15eb9f419dde6e1a7af2a8bdfdbdf66
Adam Uhlíř <adam@uhlir.dev>
2020-04-14 13:12:23 +0200

    feat(core/storage): improved error handling, logging

--- src/blockchain/utils.ts
+++ src/blockchain/utils.ts
@@ -3,11 +3,15 @@ import Eth from 'web3-eth'
 import { EventEmitter } from 'events'
 import config from 'config'
 
+import { loggingFactory } from '../logger'
 import eventsEmitterFactory, { EventsEmitterOptions, PollingOptions } from './events'
 
 export function getEventsEmitterForService (serviceName: string, eth: Eth, contractAbi: AbiItem[]): EventEmitter {
   const contractAddresses = config.get<string>(`${serviceName}.contractAddress`)
   const contract = new eth.Contract(contractAbi, contractAddresses)
+
+  const logger = loggingFactory(`${serviceName}:blockchain`)
+  logger.info(`For listening on service '${serviceName}' using contract on address: ${contractAddresses}`)
 
   const eventsToListen = config.get<string[]>(`${serviceName}.events`)
   const eventsEmitterOptions = config.get<EventsEmitterOptions>(`${serviceName}.eventsEmitter`)
