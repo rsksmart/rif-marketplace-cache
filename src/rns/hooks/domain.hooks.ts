0f776aa49282dd4cd9b0518b1b2825660ea23d41
Juraj <36888576+jurajpiar@users.noreply.github.com>
2020-05-07 18:06:44 +0100

    feat: adds query for owned and placed domains (#68)

--- src/rns/hooks/domain.hooks.ts
+++ src/rns/hooks/domain.hooks.ts
@@ -19,7 +19,51 @@ export default {
         }
       }
     ],
-    find: [],
+    find: [
+      (context: HookContext): HookContext => {
+        if (!context.params.query) {
+          context.params.query = {}
+        }
+
+        if (context.params.route?.ownerAddress) {
+          context.params.query.ownerAddress = context.params.route.ownerAddress.toLowerCase()
+        }
+        return context
+      },
+      async (context: HookContext) => {
+        if (context.params.query?.status) {
+          const { status, ownerAddress } = context.params.query
+          const statusConds: Record<string, string> = {
+            owned: '"offers"."status" != \'ACTIVE\' OR "offers"."tokenId" is null',
+            placed: '"offers"."status" = \'ACTIVE\''
+          }
+          const sql = `SELECT
+              "Domain"."tokenId",
+              "Domain"."ownerAddress",
+              "Domain"."name",
+              "Domain"."expirationDate",
+              "offers"."offerId" AS "offers.offerId",
+              "offers"."tokenId" AS "offers.tokenId",
+              "offers"."sellerAddress" AS "offers.sellerAddress",
+              "offers"."sellerDomain" AS "offers.sellerDomain",
+              "offers"."paymentToken" AS "offers.paymentToken",
+              "offers"."price" AS "offers.price",
+              "offers"."creationDate" AS "offers.creationDate",
+              "offers"."status" AS "offers.status"
+          FROM
+              "rns_domain" AS "Domain"
+              LEFT JOIN "rns_domain-offer" AS "offers" ON "Domain"."tokenId" = "offers"."tokenId"
+              
+          WHERE
+              "Domain"."ownerAddress" = '${ownerAddress}'
+              AND (${statusConds[status]});`
+
+          const sequelize = context.app.get('sequelize')
+          context.result = (await sequelize.query(sql))[0]
+        }
+        return context
+      }
+    ],
     get: [],
     create: disallow(),
     update: disallow(),
