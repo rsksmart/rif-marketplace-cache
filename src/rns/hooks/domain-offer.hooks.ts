e1117d7bead0e302da7f999072af0312dc997484
Juraj <36888576+jurajpiar@users.noreply.github.com>
2020-05-12 11:22:38 +0100

    feat: enables searching my name (#83)

--- src/rns/hooks/domain-offer.hooks.ts
+++ src/rns/hooks/domain-offer.hooks.ts
@@ -2,6 +2,7 @@ import { HookContext } from '@feathersjs/feathers'
 import { disallow } from 'feathers-hooks-common'
 
 import Domain from '../models/domain.model'
+import { Op } from 'sequelize'
 
 export default {
   before: {
@@ -20,7 +21,32 @@ export default {
         context.params.query.status = 'ACTIVE'
       }
     ],
-    find: [],
+    find: [
+      (context: HookContext) => {
+        context.params.sequelize = {
+          raw: false,
+          nest: true,
+          include: {
+            model: Domain
+          }
+        }
+        const { params: { sequelize: { include } } } = context
+        const { domain } = context.params.query as any
+
+        if ((context.params.query as any).domain) {
+          delete (context.params.query as any).domain
+        }
+
+        if (include && domain) {
+          const { name: { $like } } = domain
+          include.where = {
+            name: {
+              [Op.like]: $like
+            }
+          }
+        }
+      }
+    ],
     get: [],
     create: disallow(),
     update: disallow(),
