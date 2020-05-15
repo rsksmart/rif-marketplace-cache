c177ec312092cd1ac0c1a3e797f452f37a7e4c21
Adam Uhlíř <adam@uhlir.dev>
2020-05-15 10:26:32 +0200

    feat(core): logging improvements (#97)
    Reduced Debug DB logging and formatting SQL queries.

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,10 +1,22 @@
 import { Sequelize, SequelizeOptions } from 'sequelize-typescript'
-import { Application } from './definitions'
-import { loggingFactory } from './logger'
 import path from 'path'
 import config from 'config'
+import sqlFormatter from 'sql-formatter'
+
+import { Application } from './definitions'
+import { loggingFactory } from './logger'
 
 const logger = loggingFactory('db')
+
+function formatLogs (msg: string): string {
+  const result = msg.match(/^Executing \((\w+)\): (.*)/m)
+
+  if (!result) {
+    return msg
+  }
+
+  return `Executing SQL (${result[1]}):\n${sqlFormatter.format(result[2])}`
+}
 
 export function sequelizeFactory (): Sequelize {
   const dbSettings: SequelizeOptions = {
@@ -12,7 +24,7 @@ export function sequelizeFactory (): Sequelize {
     modelMatch: (filename: string, member: string): boolean => {
       return filename.substring(0, filename.indexOf('.model')) === member.toLowerCase()
     },
-    logging: logger.debug
+    logging: (msg) => logger.debug(formatLogs(msg))
   }
 
   return new Sequelize(config.get('db'), dbSettings)
