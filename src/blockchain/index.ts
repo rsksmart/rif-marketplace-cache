cb8b8524ec50467729701f32db3e9514ea44e700
Adam Uhlíř <adam@uhlir.dev>
2020-06-10 12:21:25 +0200

    fix: ws emits to be emitted for confirmation service

--- src/blockchain/index.ts
+++ src/blockchain/index.ts
@@ -2,7 +2,7 @@ import Eth from 'web3-eth'
 import config from 'config'
 
 import ConfirmationService from './confirmation.service'
-import { Application } from '../definitions'
+import { Application, ServiceAddresses } from '../definitions'
 import { loggingFactory } from '../logger'
 
 const logger = loggingFactory('blockchain')
@@ -14,8 +14,23 @@ export function ethFactory (): Eth {
   return new Eth(provider)
 }
 
+const CHANNEL_NAME = 'confirmations'
+
+function channelSetup (app: Application): void {
+  if (typeof app.channel !== 'function') {
+    // If no real-time functionality has been configured just return
+    return
+  }
+  app.on('connection', (connection: any) => {
+    app.channel(CHANNEL_NAME).join(connection)
+  })
+  app.service(ServiceAddresses.CONFIRMATIONS).publish(() => app.channel(CHANNEL_NAME))
+}
+
 export default function (app: Application): void {
   const eth = ethFactory()
   app.set('eth', eth)
-  app.use('/confirmations', new ConfirmationService(eth))
+  app.use(ServiceAddresses.CONFIRMATIONS, new ConfirmationService(eth))
+
+  channelSetup(app)
 }
