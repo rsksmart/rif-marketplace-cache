4e11d28b8bbdcc0067ce93f6d903227a5fdf8ade
Juraj Piar <juraj@iovlabs.org>
2020-07-24 16:08:52 +0100

    refactor: extracts filter fn for channels

--- src/services/authentication.ts
+++ src/services/authentication.ts
@@ -9,25 +9,25 @@ declare module '../definitions' {
 }
 
 class AnonymousStrategy extends AuthenticationBaseStrategy {
-    authenticate(authentication: AuthenticationRequest, params: Params) {
-        const channels: [] = authentication.channels
-        params.connection.ownerAddress = authentication.ownerAddress
-        channels.forEach(channel => {
+  authenticate (authentication: AuthenticationRequest, params: Params) {
+    const channels: [] = authentication.channels
+    params.connection.ownerAddress = authentication.ownerAddress
+    channels.forEach(channel => {
             this.app?.channel(channel).join(params.connection)
-        })
+    })
 
-        return Promise.resolve({ anonymous: true })
-    }
+    return Promise.resolve({ anonymous: true })
+  }
 }
 
 export default function (app: Application) {
-    const authentication = new AuthenticationService(app as any)
+  const authentication = new AuthenticationService(app as any)
 
-    app.set('authentication', {
-        secret: '8a58b86565c23c9ea90',
-        entity: null,
-        authStrategies: ['anonymous']
-    })
-    authentication.register('anonymous', new AnonymousStrategy())
-    app.use('/authentication', authentication)
+  app.set('authentication', {
+    secret: '8a58b86565c23c9ea90',
+    entity: null,
+    authStrategies: ['anonymous']
+  })
+  authentication.register('anonymous', new AnonymousStrategy())
+  app.use('/authentication', authentication)
 }
