b807b8daea43e7d103ce908e8a7e8026987efad2
Juraj Piar <juraj@iovlabs.org>
2020-07-24 11:55:43 +0100

    feat: filters owners for domains - needs cleaning

--- src/services/rns/rns.channels.ts
+++ src/services/rns/rns.channels.ts
@@ -8,10 +8,24 @@ export default function (app: Application) {
   }
   app.on('connection', (connection: any) => {
     app.channel('offers').join(connection)
-    app.channel('domains').join(connection)
+    // app.channel('domains').join(connection)
     app.channel('sold').join(connection)
   })
+
+  app.service(ServiceAddresses.RNS_DOMAINS).publish((data, context) => {
+    // Filter the channels to only authenticated
+    return app.channel(`domains`)
+      .filter(connection => {
+        const connectedAccount: string = connection.ownerAddress
+        const dataAccount: string = data.ownerAddress
+        console.log('----------------------')
+        console.log('rns.channels.ts -> domains publish -> connectedAccount:', connectedAccount)
+        console.log('rns.channels.ts -> domains publish -> dataAccount:', dataAccount)
+        console.log('----------------------')
+        return connectedAccount && dataAccount && connectedAccount.toLowerCase() === dataAccount.toLowerCase()
+      })
+  })
   app.service(ServiceAddresses.RNS_OFFERS).publish(() => app.channel('offers'))
-  app.service(ServiceAddresses.RNS_DOMAINS).publish(() => app.channel('domains'))
+  // app.service(ServiceAddresses.RNS_DOMAINS).publish(() => app.channel('domains'))
   app.service(ServiceAddresses.RNS_SOLD).publish(() => app.channel('sold'))
 }
