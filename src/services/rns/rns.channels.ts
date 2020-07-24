7f18986a8085d34a9bb249888a9cabdba677a0b5
Juraj Piar <juraj@iovlabs.org>
2020-07-24 16:08:52 +0100

    refactor: extracts filter fn for channels

--- src/services/rns/rns.channels.ts
+++ src/services/rns/rns.channels.ts
@@ -1,6 +1,18 @@
 import '@feathersjs/transport-commons'
 import { Application, ServiceAddresses } from '../../definitions'
 
+type RnsChannelName = 'domains' | 'sold' | 'offers'
+
+function filterByOwner (app: Application, data: any, channelName: RnsChannelName) {
+  return app.channel(channelName)
+    .filter(connection => {
+      const connectedAccount: string = connection.ownerAddress
+      const dataAccount: string = data.ownerAddress
+
+      return connectedAccount && dataAccount && connectedAccount.toLowerCase() === dataAccount.toLowerCase()
+    })
+}
+
 export default function (app: Application) {
   if (typeof app.channel !== 'function') {
     // If no real-time functionality has been configured just return
@@ -12,21 +24,10 @@ export default function (app: Application) {
   })
 
   app.service(ServiceAddresses.RNS_DOMAINS).publish((data) => {
-    return app.channel('domains')
-      .filter(connection => {
-        const connectedAccount: string = connection.ownerAddress
-        const dataAccount: string = data.ownerAddress
-
-        return connectedAccount && dataAccount && connectedAccount.toLowerCase() === dataAccount.toLowerCase()
-      })
+    return filterByOwner(app, data, 'domains')
   })
   app.service(ServiceAddresses.RNS_SOLD).publish((data) => {
-    return app.channel('domains')
-      .filter(connection => {
-        const connectedAccount: string = connection.ownerAddress
-        const dataAccount: string = data.ownerAddress
-        return connectedAccount && dataAccount && connectedAccount.toLowerCase() === dataAccount.toLowerCase()
-      })
+    return filterByOwner(app, data, 'sold')
   })
   app.service(ServiceAddresses.RNS_OFFERS).publish(() => app.channel('offers'))
 }
