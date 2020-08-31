5adcd9015cdc82972b29f5e239aa9e3dcf399479
Nazar Duchak <nazar@iovlabs.org>
2020-08-31 14:31:11 +0300

    chore: fix index file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import { loggingFactory } from './logger'
 import { appFactory } from './app'
 
 (async function (): Promise<void> {
-  const logger = loggingFactory()
-  const app = await appFactory({ appResetCallBack: () => { throw new Error('Reset callback not implemented') } })
+  await appFactory({ appResetCallBack: () => { throw new Error('Reset callback not implemented') } })
 })()
