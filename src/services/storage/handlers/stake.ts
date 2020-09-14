9126c132153d9a6d5ae3cc6a77903ddf8232d519
Nazar Duchak <nazar@iovlabs.org>
2020-09-14 18:21:07 +0300

    feat: resolve token name before creating stake row

--- src/services/storage/handlers/stake.ts
+++ src/services/storage/handlers/stake.ts
@@ -5,18 +5,97 @@ import { loggingFactory } from '../../../logger'
 import { Handler } from '../../../definitions'
 import { StorageServices } from '../index'
 import StakeModel from '../models/stake.model'
+import { Eth } from 'web3-eth'
+import { AbiItem } from 'web3-utils'
 
 const logger = loggingFactory('storage:handler:stake')
 
+// TODO replace with some import
+const ERC20Abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: 'name',
+    outputs: [
+      {
+        name: '',
+        type: 'string'
+      }
+    ],
+    payable: false,
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'decimals',
+    outputs: [
+      {
+        name: '',
+        type: 'uint8'
+      }
+    ],
+    payable: false,
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: '_owner',
+        type: 'address'
+      }
+    ],
+    name: 'balanceOf',
+    outputs: [
+      {
+        name: 'balance',
+        type: 'uint256'
+      }
+    ],
+    payable: false,
+    type: 'function'
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'symbol',
+    outputs: [
+      {
+        name: '',
+        type: 'string'
+      }
+    ],
+    payable: false,
+    type: 'function'
+  }
+]
+
+// Native token address
+export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+function getTokenName (token: string, eth: Eth, tokenAbi = ERC20Abi): string {
+  if (token === ZERO_ADDRESS) return 'RBTC'
+
+  const contract = new eth.Contract(tokenAbi as AbiItem[], token)
+  return contract.methods.symbol()
+}
+
+async function findOrCreateStake (account: string, token: string, eth: Eth): Promise<StakeModel> {
+  const stake = await StakeModel.findOne({ where: { account, token } })
+
+  if (stake) {
+    return stake
+  }
+  const tokenName = await getTokenName(token, eth)
+  return StakeModel.create({ account, token, tokenName, total: 0 })
+}
+
 const handlers = {
-  async Staked (event: EventData, { stakeService }: StorageServices): Promise<void> {
+  async Staked (event: EventData, { stakeService }: StorageServices, eth: Eth): Promise<void> {
     const { user: account, total, token, amount } = event.returnValues
 
-    const [stake] = await StakeModel.findOrCreate({
-      // TODO make a call to token contract for resolving token name
-      defaults: { account, token, total: 0, tokenName: null },
-      where: { token, account }
-    })
+    const stake = await findOrCreateStake(account, token, eth)
 
     stake.total = new BigNumber(stake.total).plus(amount)
     await stake.save()
@@ -52,12 +131,12 @@ function isValidEvent (value: string): value is keyof typeof handlers {
 
 const handler: Handler<StorageServices> = {
   events: ['Staked', 'Unstaked'],
-  process (event: EventData, services: StorageServices): Promise<void> {
+  process (event: EventData, services: StorageServices, eth: Eth): Promise<void> {
     if (!isValidEvent(event.event)) {
       return Promise.reject(new Error(`Unknown event ${event.event}`))
     }
 
-    return handlers[event.event](event, services)
+    return handlers[event.event](event, services, eth)
   }
 }
 
