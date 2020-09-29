4916fd5f3bfaffc1d342aa09c1d1a9eac7e17c7b
Nazar Duchak <nazar@iovlabs.org>
2020-09-29 22:45:27 +0300

    chore: fix sonar cloud

--- src/services/storage/index.ts
+++ src/services/storage/index.ts
@@ -49,7 +49,7 @@ export class AvgBillingPriceService extends Service {
     const sequelize = this.Model.sequelize
 
     const [{ avgPrice }] = await sequelize.query(getAvgMinMaxBillingPriceQuery(minMax), { type: QueryTypes.SELECT, raw: true })
-    return minMax === -1 ? avgPrice : avgPrice
+    return avgPrice
   }
 }
 
