f132f1cf54b71cf6bb9a4d59fb5776525a82f3bf
Nazar Duchak <nazar@iovlabs.org>
2020-09-30 13:32:05 +0300

    chore: remove todo

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -28,7 +28,6 @@ export function sequelizeFactory (): Sequelize {
     },
     logging: (msg) => logger.debug(formatLogs(msg)),
     // @ts-ignore
-    // TODO what the reason of doing this???
     transactionType: 'IMMEDIATE'
   }
 
