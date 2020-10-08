f997004a00a7b5d696291de3e75e2f6e4dada9ef
Nazar Duchak <nazar@iovlabs.org>
2020-10-08 16:18:09 +0300

    feat: add subscription for offers rooms

--- src/services/storage/communication/index.ts
+++ src/services/storage/communication/index.ts
@@ -3,10 +3,16 @@ import { createLibP2P, Room } from '@rsksmart/rif-communications-pubsub'
 import type Libp2p from 'libp2p'
 
 import { loggingFactory } from '../../../logger'
+import Offer from '../models/offer.model'
+import NotificationModel from '../../notification/notification.model'
 
 const logger = loggingFactory('storage:communication')
 
 let libp2p: Libp2p
+
+function getRoomTopic (offerId: string): string {
+  return `${config.get<string>('blockchain.networkId')}:${offerId}`
+}
 
 async function initCommunication (): Promise<void> {
   if (libp2p) {
@@ -17,14 +23,25 @@ async function initCommunication (): Promise<void> {
   logger.info('Spawn libp2p node')
 
   libp2p = await createLibP2P(libp2pConf)
+}
 
-  const room = new Room(libp2p, '0xtestroom')
-  room.on('peer:joined', (peer) => console.log(`Peer ${peer} joined.`))
-  room.on('peer:left', (peer) => console.log(`Peer ${peer} left.`))
-  room.on('message', (message) => console.log(`Message received from ${message.from}, message ${message.data}`))
-  room.on('error', (e) => console.log(e))
+async function handleMessage (message: any): Promise<void> {
+  // await NotificationModel.create({ title: 'Some titile',})
 }
 
 async function subscribeForOffers () {
+  const offers = await Offer.findAll()
+  offers.forEach(offer => {
+    const topic = getRoomTopic(offer.provider)
+    const logger = loggingFactory(`storage:communication:room:${topic}`)
+    const room = new Room(libp2p, topic)
 
+    room.on('message', async ({ from, data }) => {
+      if (from !== offer.peerId) {
+        return
+      }
+      await handleMessage(data)
+    })
+    room.on('error', (e) => logger.error(e))
+  })
 }
