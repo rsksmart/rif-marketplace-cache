1ed418b8b3ee26fdd5154f6ccf6198f1c394027d
Adam Uhlíř <adam@uhlir.dev>
2020-10-29 10:00:51 +0100

    test: fixing precision mismatch

--- test/integration/services/storage/storage-manger.spec.ts
+++ test/integration/services/storage/storage-manger.spec.ts
@@ -306,7 +306,7 @@ describe('Storage service', function () {
         const rate = await Rate.findOne({ where: { token: 'rbtc' }, raw: true })
         const [stake] = await StakeModel.findAll({ where: { account }, raw: true })
 
-        const totalStakedUSD = (new BigNumber(stake.total)).div(WEI).multipliedBy(rate?.usd as number).toString()
+        const totalStakedUSD = (new BigNumber(stake.total)).div(WEI).multipliedBy(rate?.usd as number).toFixed(2).toString()
         const { totalStakedFiat, stakes } = await stakeService.get(account)
         expect(totalStakedUSD).to.be.eql(totalStakedFiat)
         expect(stakes.length).to.be.eql(1)
