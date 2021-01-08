8edfa3f62644b26c67f98c1bc1a0a750d04d3173
Nazar Duchak <nazar@iovlabs.org>
2021-01-08 12:40:45 +0200

    feat: poc for notification endpoint

--- src/notification/index.ts
+++ src/notification/index.ts
@@ -1,12 +1,27 @@
 import { Service } from 'feathers-sequelize'
 
-import { Application, ServiceAddresses } from '../definitions'
+import { Application, CommsMessage, CommsPayloads, MessageHandler, ServiceAddresses } from '../definitions'
 import notificationHooks from './notification.hooks'
 import notificationChannels from './notification.channels'
 import NotificationModel from './notification.model'
+import { messageHandler } from '../communication/handlers'
 
 export class NotificationService extends Service {
   emit?: Function
+  messageHandler: MessageHandler
+
+  constructor(config, notificationService: NotificationService) {
+    super(config);
+    this.messageHandler = messageHandler(notificationService)
+  }
+
+  async create (offerId: string, message: CommsMessage<CommsPayloads>): Promise<void> {
+    // TODO check somehow that message come from the correct pinner
+    // Maybe add some message encryption using libp2p keys
+
+    await this.messageHandler(message)
+    return true
+  }
 }
 
 export default function (app: Application): void {
