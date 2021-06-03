93c29fc7e50a50cac4d925a57c2e33559f5fa383
Vijay K <11134695+vjgene@users.noreply.github.com>
2021-06-03 19:53:08 +0530

    feat(notifier): edit provider (#696)
    * feat(notifier): edit provider
    
    * feat(notifier): edit provider
    
    * feat(notifier): fix tests
    
    * feat(notifier): refactor edit provider

---
+++ src/services/notifier/utils/updaterUtils.ts
@@ -0,0 +1,47 @@
+import NotifierChannelModel from '../models/notifier-channel.model'
+import PlanModel from '../models/plan.model'
+import PriceModel from '../models/price.model'
+import { NotifierSvcProvider, SubscriptionPlanDTO } from '../notifierService/provider'
+
+function deactivateDeletedPlans (currentPlans:Array<PlanModel>, incomingPlans:Array<SubscriptionPlanDTO>):void {
+  if (currentPlans && incomingPlans) {
+    const deletedPlans:Array<PlanModel> = currentPlans.filter(({
+      planId: currentId,
+      name: currentName,
+      planStatus: currentPlanStatus,
+      quantity: currentQuantity,
+      daysLeft: currentValidity,
+      channels
+    }) => !incomingPlans.some(({
+      id: incomingId,
+      name: incomingName,
+      validity: incomingValidity,
+      planStatus: incomingPlanStatus,
+      notificationPreferences: incomingNotificationPreferences,
+      notificationQuantity: incomingNotificationQuantity
+    }) => (currentId === incomingId &&
+        currentName === incomingName &&
+        currentPlanStatus === incomingPlanStatus &&
+        currentQuantity === incomingNotificationQuantity &&
+        currentValidity === incomingValidity &&
+        JSON.stringify(channels.map(channel => channel.name)) === JSON.stringify(incomingNotificationPreferences)
+    )
+    ))
+
+    if (deletedPlans.length) {
+      const ids:Array<number> = deletedPlans.map(deletedPlan => deletedPlan.id)
+      PlanModel.update({ planStatus: 'INACTIVE', id: ids }, { where: { id: ids } })
+    }
+  }
+}
+
+export async function deactivateDeletedPlansForProvider (provider: string, url: string): Promise<void> {
+  const currentPlans = await PlanModel.findAll({
+    where: { providerId: provider },
+    include: [{ model: NotifierChannelModel }, { model: PriceModel }]
+  })
+  const [host, port] = url.split(/(?::)(\d*)$/, 2)
+  const svcProvider = new NotifierSvcProvider({ host, port })
+  const { content: incomingPlans } = await svcProvider.getSubscriptionPlans() || {}
+  deactivateDeletedPlans(currentPlans, incomingPlans)
+}
