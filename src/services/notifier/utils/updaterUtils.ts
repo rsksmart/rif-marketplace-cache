bcd679e671158f0ffae28ce6ac7cda8ca02704a1
Alvaro Fariña <farinalvaro@gmail.com>
2021-07-13 08:56:29 -0300

    feat(notifier): updates or creates subscriptions while fetching (#740)
    Co-authored-by: Vijay Krishna <11134695+vjgene@users.noreply.github.com>

--- src/services/notifier/utils/updaterUtils.ts
+++ src/services/notifier/utils/updaterUtils.ts
@@ -1,9 +1,13 @@
+import BigNumber from 'bignumber.js'
+import { SupportedServices } from '../../../definitions'
 import { loggingFactory } from '../../../logger'
+import { getTokenSymbol } from '../../utils'
 import NotifierChannelModel from '../models/notifier-channel.model'
 import PlanModel from '../models/plan.model'
 import PriceModel from '../models/price.model'
+import ProviderModel from '../models/provider.model'
 import SubscriptionModel from '../models/subscription.model'
-import { NotifierSvcProvider, SubscriptionPlanDTO } from '../notifierService/provider'
+import { NotifierSvcProvider, SubscriptionDTO, SubscriptionPlanDTO } from '../notifierService/provider'
 
 const logger = loggingFactory('notifier:updaterUtils')
 
@@ -50,25 +54,93 @@ export async function deactivateDeletedPlansForProvider (provider: string, url: 
   deactivateDeletedPlans(currentPlans, incomingPlans)
 }
 
+export const buildSubscriptionFromDTO = async (subscriptionDTO: SubscriptionDTO, provider: string): Promise<any> => {
+  const {
+    currency: {
+      address: {
+        value: tokenAddress
+      }
+    },
+    price,
+    expirationDate,
+    id: subscriptionId,
+    subscriptionPlanId,
+    previousSubscription: previousSubscriptionModel,
+    userAddress: consumer,
+    hash,
+    notificationBalance,
+    paid,
+    signature,
+    status,
+    topics
+  } = subscriptionDTO
+
+  const rateId = getTokenSymbol(tokenAddress, SupportedServices.NOTIFIER).toLowerCase()
+
+  const plan = await PlanModel.findOne({
+    where: {
+      planId: subscriptionPlanId,
+      planStatus: 'ACTIVE',
+      providerId: provider
+    }
+  })
+
+  if (!(plan && rateId)) throw new Error('Bad SubscriptioDTO object')
+
+  return {
+    rateId,
+    providerId: provider,
+    subscriptionId,
+    subscriptionPlanId: plan.id,
+    price: new BigNumber(price),
+    expirationDate: new Date(expirationDate),
+    previousSubscription: previousSubscriptionModel?.hash,
+    consumer,
+    hash,
+    notificationBalance,
+    paid,
+    signature,
+    status,
+    topics
+  }
+}
+
+const findOrCreateSubscription = async (subscriptionDTO: SubscriptionDTO, url: string) => {
+  const { hash, status, paid, notificationBalance, expirationDate } = subscriptionDTO
+  const found = await SubscriptionModel.findOne({ where: { hash } })
+
+  if (found) {
+    return SubscriptionModel.update(
+      { status, paid, notificationBalance, expirationDate },
+      { where: { hash } })
+  }
+  const providerModel = await ProviderModel.findOne({ where: { url } })
+  const provider = providerModel?.provider
+
+  if (!provider) {
+    throw new Error(`Provider model with url ${url} not found.`)
+  }
+
+  const subscription = await buildSubscriptionFromDTO(subscriptionDTO, provider)
+  return SubscriptionModel.create(subscription)
+}
+
 export const updateSubscriptionsBy = async (
-  providerUrl: string, consumerAddress: string, subsHashesToUpdate: string[]
-): Promise<void> => {
+  providerUrl: string, consumerAddress: string): Promise<void> => {
   const [host, port] = providerUrl.split(/(?::)(\d*)$/, 2)
   const svcProvider = new NotifierSvcProvider({ host, port })
   try {
-    const subscriptionsDTO: any[] = await svcProvider.getSubscriptions(consumerAddress, subsHashesToUpdate)
-    await Promise.all(subscriptionsDTO.map(({
-      hash,
-      status,
-      paid,
-      notificationBalance,
-      expirationDate
-    }) =>
-      SubscriptionModel.update(
-        { status, paid, notificationBalance, expirationDate },
-        { where: { hash } }
-      ).catch(error => logger.warn(`Unable to update subscription with hash ${hash} in the database`, error))
-    ))
+    const subscriptionsDTO: Array<SubscriptionDTO> = await svcProvider.getSubscriptions(consumerAddress)
+
+    await Promise.all(
+      subscriptionsDTO.map(
+        (subscription) => findOrCreateSubscription(
+          subscription, providerUrl
+        ).catch(
+          error => logger.warn(`Unable to update or create subscription with hash ${subscription.hash} in the database`, error)
+        )
+      )
+    )
   } catch (error) {
     logger.warn('Unable to update subscriptions from notifier service provider', error)
   }
